373be36a2160690121509902e2ed84770851f15e
Griffin Dooley <dooleygriffin@gmail.com>
2022-06-21 07:25:58 -0400

    added browserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <p>LETS GOOOOO</p>
-      </header>
+      <Route exact path="/" >
+        <header className="App-header">
+          <p>LETS GOOOOO</p>
+        </header>
+      </Route>
+      <Route path="/homepage" >
+        <Homepage />
+      </Route>
     </div>
   );
 }
