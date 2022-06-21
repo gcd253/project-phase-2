06ec003baf511c88344d847b9a0f1ab6fadb6616
Griffin Dooley <dooleygriffin@gmail.com>
2022-06-21 11:02:47 -0400

    added navBar functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AddChamp from './components/AddChamp';
 import Homepage from './components/Homepage';
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 import Streams from './components/Streams';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Route exact path="/" >
         <header className="App-header">
           <p>LETS GOOOOO</p>
